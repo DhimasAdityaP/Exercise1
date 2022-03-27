d60d6b6dd6f5aee24f3f453ed782490d7a2bba1a
cindynrh <cindynurhabibah@gmail.com>
2022-03-27 22:11:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/exercise1/activity_result.java
@@ -0,0 +1,32 @@
+package com.example.exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class activity_result extends AppCompatActivity {
+    TextView txTask, txJenis, txTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        txTask = findViewById(R.id.tvTask);
+        txJenis = findViewById(R.id.tvJenistask);
+        txTime  = findViewById(R.id.tvTimetask);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String task = bundle.getString("task");
+        String jenis = bundle.getString("jenis");
+        String time = bundle.getString("time");
+
+        txTask.setText(task);
+        txJenis.setText(jenis);
+        txTime.setText(time);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/exercise1/activity_task.java
@@ -0,0 +1,72 @@
+package com.example.exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class activity_task extends AppCompatActivity {
+
+    TextView Nama,txTask, txJenis, txTime;
+
+    FloatingActionButton apa;
+    String task,jenis,time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+        txTask = findViewById(R.id.tvTask);
+        txJenis = findViewById(R.id.tvJenistask);
+        txTime  = findViewById(R.id.tvTimetask);
+        apa = findViewById(R.id.faButton);
+
+        Nama = findViewById(R.id.tvusername);
+
+
+        Bundle bundle = getIntent().getExtras();
+
+        String Indra = bundle.getString("ab");
+
+        Nama.setText(Indra);
+
+        apa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                task = txTask.getText().toString();
+                jenis = txJenis.getText().toString();
+                time = txTime.getText().toString();
+
+                if (txTask.getText().toString().isEmpty()||txJenis.getText().toString().isEmpty()
+                        ||txTime.getText().toString().isEmpty()){
+                    txJenis.setError("JEnis Task!");
+                    txTime.setError("Lama Task!");
+                    Toast.makeText(getApplicationContext(), "Data harus di isi", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_SHORT).show();
+                    Bundle ya = new Bundle();
+
+                    ya.putString("task",txTask.getText().toString().trim());
+                    ya.putString("jenis", txJenis.getText().toString().trim());
+                    ya.putString("time", txTime.getText().toString().trim());
+
+                    Intent lo = new Intent(getApplicationContext(), activity_result.class);
+
+                    lo.putExtras(ya);
+                    startActivity(lo);
+
+                }
+
+            }
+        });
+
+    }
+}
